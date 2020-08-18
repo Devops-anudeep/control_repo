1a1d79a2e5fc451d3a1adc304afa36cca965d492
anudeep-onshape <69157924+anudeep-onshape@users.noreply.github.com>
2020-08-18 18:27:28 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 node default {
+}
+
+node 'master.puppet.vm' {
+  include role::master_server
   file {'/root/README':
     ensure  => file,
     content => $os,
     owner   => 'root',
   }
-}
-
-node 'master.puppet.vm' {
-  include role::master_server
 }
 
 
