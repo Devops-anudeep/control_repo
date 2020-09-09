76cc2764f05910afda0e39a62fb5d4075ee9bdf8
anudeep-onshape <69157924+anudeep-onshape@users.noreply.github.com>
2020-09-09 10:06:43 -0400

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,13 @@ class profile::base {
   user { 'admin':
     ensure => present,
   }
+  
+  $facts['disks'].each |$disk|{
+    exec {"dynamic scheduler change for ${disk}" :
+      command     => "touch /tmp/test/\$disk",
+      path        => '/usr/bin',
+      provider    => 'shell',
+      refreshonly => true
+    }
+  }
 }
